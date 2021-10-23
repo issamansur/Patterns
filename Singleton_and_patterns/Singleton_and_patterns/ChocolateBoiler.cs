1332fd3d3ef3b6ad03c61c8a896e56b20d0cc6e4
User#None <77580844+issamansur@users.noreply.github.com>
2021-10-23 12:17:44 +0300

    Add files via upload

---
+++ Singleton_and_patterns/Singleton_and_patterns/ChocolateBoiler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton_and_patterns
+{
+    public class ChocolateBoiler
+    {
+        private static
+            ChocolateBoiler uniqueInstance;
+
+        private static object 
+            syncRoot = new Object();
+
+        /*u can don't use lock and syncRoot if do this:
+            private static ChocolateBoiler uniqueInstance = 
+                new ChocolateBoiler();
+        */
+
+        public static ChocolateBoiler GetInstance()
+        {
+            if (uniqueInstance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (uniqueInstance == null)
+                        uniqueInstance = new ChocolateBoiler();
+                }
+            }
+
+            return uniqueInstance;
+        }
+
+        private bool empty;
+        private bool boiled;
+
+        private ChocolateBoiler()
+        {
+            empty = true;
+            boiled = false;
+        }
+
+        public void Fill()
+        {
+            if (empty)
+            {
+                empty = boiled = false;
+            }
+        }
+
+        public void Drain()
+        {
+            if (!empty && boiled)
+                empty = true;
+        }
+
+        public void Boil()
+        {
+            if (!empty && !boiled)
+                boiled = true;
+        }
+    }
+}
---
+++ Singleton_and_patterns/Singleton_and_patterns/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Singleton_and_patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChocolateBoiler boiler =
+                ChocolateBoiler.GetInstance();
+            boiler.Fill();
+            boiler.Boil();
+            boiler.Drain();
+        }
+    }
+}
